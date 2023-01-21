73eea39e36cc197d0262775ad9820aa726ffccc7
knightron0 <sarthak.robo@gmail.com>
2023-01-21 10:31:50 -0500

    update flask

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,13 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "hi!"
+@app.route('/report', methods = ["GET", "POST"])
+def report():
+    if request.method == "POST":
+        print(request.headers)
+    
+    return "hi"
+
 
 if __name__ == "__main__":
     app.run(host= "0.0.0.0", debug=True)
