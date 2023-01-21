9f632493e5961c349da58f69e85c1b539866e2f5
knightron0 <sarthak.robo@gmail.com>
2023-01-21 00:41:20 -0500

    add swift app

--- insight/insight/ViewController.swift
+++ insight/insight/ViewController.swift
@@ -8,11 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var reportIncident: UIButton!
+    @IBOutlet var exploreData: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor(red: CGFloat(27)/255.0, green: CGFloat(26)/255.0, blue: CGFloat(26)/255.0, alpha: 1)
+        
+//        reportIncident.titleLabel?.font = UIFont.boldSystemFont(ofSize: 19.0);
+//        reportIncident.titleLabel?.textColor = UIColor(red: CGFloat(57)/255.0, green: CGFloat(57)/255.0, blue: CGFloat(57)/255.0, alpha: 1)
+        reportIncident.backgroundColor = UIColor(red: CGFloat(255)/255.0, green: CGFloat(189)/255.0, blue: CGFloat(90)/255.0, alpha: 1)
+        reportIncident.layer.cornerRadius = 10
+        
+//        exploreData.titleLabel?.font = UIFont.boldSystemFont(ofSize: 19.0);
+//        exploreData.titleLabel?.textColor = UIColor(red: CGFloat(255)/255.0, green: CGFloat(189)/255.0, blue: CGFloat(90)/255.0, alpha: 1)
+        exploreData.layer.borderColor = CGColor(red: CGFloat(255)/255.0, green: CGFloat(189)/255.0, blue: CGFloat(90)/255.0, alpha: 1)
+        exploreData.layer.borderWidth = 2.0
+        exploreData.layer.cornerRadius = 10
+        
     }
+
 
 
 }
