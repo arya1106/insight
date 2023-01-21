4e629d0d2087d47485c30699a4626ce3be33c4be
Aryamaan <aryamaan.dhomne@gmail.com>
2023-01-21 17:06:58 +0000

    fix pred script

--- backend/predictions.py
+++ backend/predictions.py
@@ -5,10 +5,11 @@ import json
 CLASS_MAPPINGS={0: "long_crack", 1: "trans_crack", 2: "aligator_crack", 3: "pothole"}
 
 def predict():
-    model = YOLO("best.pt")
+    model = YOLO("../ML/train4/weights/best.pt")
     outList = []
 
     results = model.predict(source=sys.argv[1])
+    print(sys.argv[1])
     if results:    
         boxCords = results[0].boxes.xyxy
         classes = results[0].boxes.cls
@@ -23,3 +24,5 @@ def predict():
     jsonString = json.dumps(outDict)
     print(jsonString)
 
+if __name__ == "__main__":
+    predict()
