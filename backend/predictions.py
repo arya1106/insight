9a9b3968e2607dc88f4a1e580eee5080513398a2
Aryamaan <aryamaan.dhomne@gmail.com>
2023-01-21 09:38:57 -0500

    fomratting

--- backend/predictions.py
+++ backend/predictions.py
@@ -4,21 +4,22 @@ import json
 
 CLASS_MAPPINGS={0: "long_crack", 1: "trans_crack", 2: "aligator_crack", 3: "pothole"}
 
-model = YOLO("best.pt")
-outList = []
+def predict():
+    model = YOLO("best.pt")
+    outList = []
 
-results = model.predict(source=sys.argv[1])
-if results:    
-    boxCords = results[0].boxes.xyxy
-    classes = results[0].boxes.cls
+    results = model.predict(source=sys.argv[1])
+    if results:    
+        boxCords = results[0].boxes.xyxy
+        classes = results[0].boxes.cls
 
-    for index, element in enumerate(boxCords):
-        out = {}
-        out["boxCords"] = boxCords[index].tolist()
-        out["damageType"] = classes[index].item()
-        outList.append(out)
+        for index, element in enumerate(boxCords):
+            out = {}
+            out["boxCords"] = boxCords[index].tolist()
+            out["damageType"] = classes[index].item()
+            outList.append(out)
 
-outDict = {"damages": outList}
-jsonString = json.dumps(outDict)
-print(jsonString)
+    outDict = {"damages": outList}
+    jsonString = json.dumps(outDict)
+    print(jsonString)
 
