d4ba22faf546732cabcf4d399a382d68fd71243b
knightron0 <sarthak.robo@gmail.com>
2023-01-21 01:37:56 -0500

    add damage view controller

--- insight/insight/IncidentViewController.swift
+++ insight/insight/IncidentViewController.swift
@@ -6,14 +6,44 @@
 //
 
 import UIKit
+import CoreLocation
+
 
 class IncidentViewController: UIViewController {
 
+    @IBOutlet var submitReport: UIButton!
+    @IBOutlet var locationLabel: UILabel!
+    
+    let locationManager = CLLocationManager();
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        var currentLocation: CLLocation!
+
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse || CLLocationManager.authorizationStatus() ==  .authorizedAlways {
+            currentLocation = locationManager.location
+            print(currentLocation.coordinate.latitude)
+            print(currentLocation.coordinate.longitude)
+            let geocoder = CLGeocoder()
+            geocoder.reverseGeocodeLocation(currentLocation) { (placemarks, error) in
+                if let error = error {
+                    print(error)
+                } else if let placemarks = placemarks {
+                    for placemark in placemarks {
+                        self.locationLabel.text = "📍 " + String(placemark.locality!) + ", " + String(placemark.administrativeArea!)
+                    }
+                }
+            }
+        }
+
+        
         view.backgroundColor = UIColor(red: CGFloat(27)/255.0, green: CGFloat(26)/255.0, blue: CGFloat(26)/255.0, alpha: 1)
         
         // Do any additional setup after loading the view.
+        submitReport.backgroundColor = UIColor(red: CGFloat(255)/255.0, green: CGFloat(189)/255.0, blue: CGFloat(90)/255.0, alpha: 1)
+        submitReport.layer.cornerRadius = 10
+
     }
     
 
@@ -26,5 +56,10 @@ class IncidentViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func getAddress(fromLocation location: CLLocation) {
+        
+
+    }
 
 }
--- insight/insight/ViewController.swift
+++ insight/insight/ViewController.swift
@@ -6,14 +6,17 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
     
     @IBOutlet var reportIncident: UIButton!
     @IBOutlet var exploreData: UIButton!
+    let locationManager = CLLocationManager();
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.requestWhenInUseAuthorization()
         // Do any additional setup after loading the view.
         view.backgroundColor = UIColor(red: CGFloat(27)/255.0, green: CGFloat(26)/255.0, blue: CGFloat(26)/255.0, alpha: 1)
         
