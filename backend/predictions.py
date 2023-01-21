022f206e5c21c2a6cae99a9c962f90dedc8ae0f7
Aryamaan <aryamaan.dhomne@gmail.com>
2023-01-21 14:33:53 +0000

    bare bones prediction script

---
+++ backend/predictions.py
@@ -0,0 +1,24 @@
+from ultralytics import YOLO
+import sys
+import json
+
+CLASS_MAPPINGS={0: "long_crack", 1: "trans_crack", 2: "aligator_crack", 3: "pothole"}
+
+model = YOLO("best.pt")
+outList = []
+
+results = model.predict(source=sys.argv[1])
+if results:    
+    boxCords = results[0].boxes.xyxy
+    classes = results[0].boxes.cls
+
+    for index, element in enumerate(boxCords):
+        out = {}
+        out["boxCords"] = boxCords[index].tolist()
+        out["damageType"] = classes[index].item()
+        outList.append(out)
+
+outDict = {"damages": outList}
+jsonString = json.dumps(outDict)
+print(jsonString)
+
