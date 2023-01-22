06588524c52fa12b5cec090b2446669242607119
Leon <leon.yee000@gmail.com>
2023-01-22 00:00:42 -0500

    map scale

--- js/map.js
+++ js/map.js
@@ -8,7 +8,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-
+L.control.scale().addTo(map);
 
 //marker variable
 var marker;
